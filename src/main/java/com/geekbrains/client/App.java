535dc3e09891a2febb5565fa1598debdaf92e6b0
Михаил Левин <mikelevin@yandex-team.ru>
2021-12-14 22:00:56 +0300

    Lesson 2. Nio

--- src/main/java/com/geekbrains/client/App.java
+++ src/main/java/com/geekbrains/client/App.java
@@ -9,7 +9,7 @@ import javafx.stage.Stage;
 public class App extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent parent = FXMLLoader.load(getClass().getResource("client.fxml"));
+        Parent parent = FXMLLoader.load(getClass().getResource("cloud_client.fxml"));
         primaryStage.setScene(new Scene(parent));
         primaryStage.show();
     }
--- src/main/java/com/geekbrains/server/Handler.java
+++ src/main/java/com/geekbrains/server/Handler.java
@@ -2,46 +2,68 @@ package com.geekbrains.server;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Handler implements Runnable {
 
+    private static final int BUFFER_SIZE = 8192; // 8Kb
+
+    private Path currentDir;
+    private byte[] buffer;
     private final DataInputStream is;
     private final DataOutputStream os;
 
     public Handler(Socket socket) throws IOException {
         is = new DataInputStream(socket.getInputStream());
         os = new DataOutputStream(socket.getOutputStream());
+        currentDir = Paths.get("serverFiles");
         System.out.println("Client accepted...");
+        sendServerFiles();
+        buffer = new byte[BUFFER_SIZE];
+    }
+
+    private List<String> getFileNames() throws IOException {
+        return Files.list(currentDir)
+                .map(p -> p.getFileName().toString())
+                .collect(Collectors.toList());
+    }
+
+    private void sendServerFiles() throws IOException {
+        os.writeUTF("#list#");
+        List<String> names = getFileNames();
+        os.writeInt(names.size());
+        for (String name : names) {
+            os.writeUTF(name);
+        }
+        os.flush();
     }
 
     @Override
     public void run() {
         try {
             while (true) {
-                String command = is.readUTF().trim();
+                String command = is.readUTF();
                 System.out.println("received command: " + command);
-                switch (command) {
-                    case "getFile": {
-                        os.writeUTF("Enter file name...");
-                        String fileName = is.readUTF();
-                        os.writeUTF("File: " + fileName);
-                        break;
+                if (command.equals("#upload#")) {
+                    String fileName = is.readUTF();
+                    long size = is.readLong();
+                    try (FileOutputStream fos = new FileOutputStream(
+                            currentDir.resolve(fileName).toFile())) {
+                        for (int i = 0; i < (size + BUFFER_SIZE - 1) / BUFFER_SIZE; i++) {
+                            int read = is.read(buffer);
+                            fos.write(buffer, 0, read);
+                        }
                     }
-                    case "getListFiles":
-                        os.writeUTF("List: {File1, File2, File3}");
-                        break;
-                    case "putFile": {
-                        os.writeUTF("Enter file name");
-                        String fileName = is.readUTF();
-                        os.writeUTF("Enter file size");
-                        long size = is.readLong();
-                        os.writeUTF("Upload: file " + fileName + " uploaded, size: " + size);
-                        break;
-                    }
+                    sendServerFiles();
                 }
-                os.flush();
             }
         } catch (Exception e) {
             e.printStackTrace();
