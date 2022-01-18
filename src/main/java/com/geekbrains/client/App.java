44e92597fcdda5745e85ceff89a4d73a6825863f
Михаил Левин <mikelevin@yandex-team.ru>
2022-01-18 20:20:12 +0300

    Lesson 4. StreamApi

--- src/main/java/com/geekbrains/client/App.java
+++ src/main/java/com/geekbrains/client/App.java
@@ -7,9 +7,10 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class App extends Application {
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent parent = FXMLLoader.load(getClass().getResource("cloud_client.fxml"));
+        Parent parent = FXMLLoader.load(getClass().getResource("auth.fxml"));
         primaryStage.setScene(new Scene(parent));
         primaryStage.show();
     }
